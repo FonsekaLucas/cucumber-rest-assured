0bc2afdda1ab6048a3a571daca0b7e9da0e259e5
FonsekaLucas <fonsekalucas@gmail.com>
2023-10-01 01:22:42 -0300

    Primeiro commit de estrutura do projeto.

---
+++ src/main/java/br/com/cucumberrestassured/CucumberRestassuredApplication.java
@@ -0,0 +1,13 @@
+package br.com.cucumberrestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberRestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberRestassuredApplication.class, args);
+	}
+
+}
